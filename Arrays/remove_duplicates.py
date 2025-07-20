13b24782ba8ca14bc1cba3f867dee7cf630507ff
ashish shukla <ashishhh1226@gmail.com>
2025-07-20 20:39:19 +0530

    Add remove_duplicates.py solution

---
+++ Arrays/remove_duplicates.py
@@ -0,0 +1,7 @@
+def removeDuplicates(nums):
+        count = 0
+        for i in range(1, len(nums)):
+            if nums[count] != nums[i]:
+                count = count+1
+                nums[count] = nums[i]
+        return count+1
